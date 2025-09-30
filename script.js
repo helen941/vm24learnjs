07ff31003eb2a55cc0b77a415e6907022be139a4
Helen Ots <helenots2008@gmail.com>
2025-09-30 09:15:12 +0300

    add types examples

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
  console.log("Hello World");
+ var num = 1; // don`t use var anymore
+ let num2 = 2;
+ console.log(num2, num);
+ num = 3;
+ console.log(typeof num);
+ let text = 'Helen';
+ console.log(typeof text);
+ let bool = true;
+ console.log(typeof bool);
+ bool = false;
+ console.log(typeof bool);
+ let nothing;
+ console.log(typeof nothing);
+ nothing = undefined;
+ nothing = null;
+ console.log(typeof nothing);
+ let array = [1, 2, 3, 4, 'sad', true, 3.5];
+ console.log(typeof array);
+ let obj = {
+    name: 'Helen',
+    age: 17,
+    cat: [Kurru Nurru],
+    data. {
+        phone: '123456',
+        email: 'helenots2008@gmail.com'
+    }
+ };
+ console.log(typeof obj);
+
+ function helle(){
+    console.log('hello');
+ }
+ console.log(typeof hello);
