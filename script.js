000d528e24290a956c15361d0f5172e873c95cb8
Helen Ots <helenots2008@gmail.com>
2025-09-30 09:47:44 +0300

    Add math examples

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
- console.log("Hello World");
- var num = 1; // don`t use var anymore
- let num2 = 2;
- console.log(num2, num);
- num = 3;
- console.log(typeof num);
- let text = 'Helen';
- console.log(typeof text);
- let bool = true;
- console.log(typeof bool);
- bool = false;
- console.log(typeof bool);
- let nothing;
- console.log(typeof nothing);
- nothing = undefined;
- nothing = null;
- console.log(typeof nothing);
- let array = [1, 2, 3, 4, 'sad', true, 3.5];
- console.log(typeof array);
- let obj = {
-    name: 'Helen',
-    age: 17,
-    cat: ['Kurru Nurru'],
-    data: {
-        phone: '123456',
-        email: 'helenots2008@gmail.com'
-    }
- };
- console.log(typeof obj);
-
- function helle(){
-    console.log('hello');
- }
- console.log(typeof hello);
+console.log(1+2);
+let num = 1;
+console.log(num);
+num = num +2;
+console.log(num);
+num = num - 1;
+console.log(num);
+num = num * 2;
+console.log(num);
+num = num / 3;
+console.log(num);
+num = 3;
+num = num **3;
+console.log(num);
+num = 9;
+num = num % 2;
+console.log(num);
+num += 2;
+num -= 2;
+num *= 2;
+num /= 2;
+num **= 2;
+num %= 2
+console.log(num);
+num++;
+console.log(num);
+num--;
+console.log(num);
+Math.sqrt(9);
+console.log(num);
