9692406861fc49403a2fe0701f79e629ab9a29c6
Helen Ots <helenots2008@gmail.com>
2025-10-07 08:50:08 +0300

    Add string examples

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-console.log(1+2);
-let num = 1;
-console.log(num);
-num = num +2;
-console.log(num);
-num = num - 1;
-console.log(num);
-num = num * 2;
-console.log(num);
-num = num / 3;
-console.log(num);
-num = 3;
-num = num **3;
-console.log(num);
-num = 9;
-num = num % 2;
-console.log(num);
-num += 2;
-num -= 2;
-num *= 2;
-num /= 2;
-num **= 2;
-num %= 2
-console.log(num);
-num++;
-console.log(num);
-num--;
-console.log(num);
-Math.sqrt(9);
-console.log(num);
+let text = 'Hello' + 'World' + '!';
+console.log(text);
+text += 'yeee!';
+console.log(text);
+text = "don't do this ;P";
+text = 'don\'t do\n this\t ;P';
+console.log(text)
+text += 'dsudfigugöugohgohghgöhgölihglghlglgrhegh' +
+        'tryfyugihojpklüöärtyfughijokoptf7yugihoj' +
+        'tryfguhijopklüörtyfhyuijokpyftughijoktyu';
+let name = 'Helen';
+let age = 17;
+text = `Hello
+this is multi line
+text`;
+console.log(text);
+text = 'Hello' + name + '! You are' + age + 'years old!';
+text = `Hello ${name}! You are ${age} years old`;
+console.log(text);
+console.log(text.length) ;
+console.log(text.toUpperCase());
+
