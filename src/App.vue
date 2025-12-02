ee71ca5f9c33392520e014f1e93b2b078f556118
Helen Ots <helenots2008@gmail.com>
2025-12-02 09:57:43 +0200

    Add modal examples

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,20 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { ref } from 'vue'
+import Modal from './components/Modal.vue'
 
-let message = ref('Hello Vue');
-let isDanger = ref(false)
-let newItem = ref('Konjak');
-let i = 0;
-let items = ref([
-  {id: i++, name:'Alma kohuke', isDone: true},
-  {id: i++, name:'Jänksi kohuke', isDone: false},
-  {id: i++, name:'Tere kohuke', isDone: true},
-  {id: i++, name:'Alma piim', isDone: false},
-    ]);
-
-function add(){
-  if(newItem.value.trim() !== ''){
-  items.value.push({id: i++, name: newItem.value, isDone: false});
-  }
-  newItem.value = '';
-}
-
-let doneItems = computed(() => items.value.filter(item => item.isDone));
-
+let modal1Active = ref(false);
+let modal2Active = ref(false);
 </script>
 
 <template>
-    <div class="container mt-2">
-        <div class="content">
-            <button class="button is-primary" @click="message = 'Hello VM24'">Click me!</button>
-            <h1>{{ message }}</h1>
-            <button class="button" :class="{ 'is-danger': isDanger, 'is-warning': !isDanger }"
-                @click="isDanger = !isDanger">Cool button</button>
+  <div class="container mt-2">
+    <div class="content">
+      <button class="button is-primary" @click="modal1Active = true">Open Modal 1</button>
+      <button class="button is-link" @click="modal2Active = true">Open Modal 2</button>
 
-            <div class="field has-addons mt-2">
-                <div class="control is-expanded">
-                    <input v-model="newItem" @keydown.enter="add"class="input" type="text" placeholder="Add item">
-                </div>
-                <div class="control">
-                    <button class="button is-info" @click="add">
-                        Add Item
-                    </button>
-                </div>
-            </div>
-            <h1>{{ newItem.split('').reverse().join('') }}</h1>
-
-            <h1>All Items</h1>
-            <ul>
-              <li v-for="item in items">
-                {{ item.name }}
-                <input type = "checkbox" v-model="item.isDone">
-              </li>
-            </ul>
-
-             <h1>Done Items</h1>
-            <ul>
-              <li v-for="item in doneItems">
-                {{ item.name }}
-                <input type = "checkbox" v-model="item.isDone">
-              </li>
-            </ul>
-
-
-        </div>
+      <Modal :active="modal1Active" @close="modal1Active = false" url="https://picsum.photos/1290/960?random=1"> </Modal>
+      <Modal :active="modal2Active" @close="modal1Active = false" url="https://picsum.photos/1290/960?random=2"> </Modal>
+    
     </div>
+  </div>
 </template>
