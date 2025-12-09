6fd874dd95aa17b0bf40cb2bce44b4e5e7088340
Helen Ots <helenots2008@gmail.com>
2025-12-09 08:43:10 +0200

    Add slot examples

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Modal from './components/Modal.vue'
 
 let modal1Active = ref(false);
 let modal2Active = ref(false);
+let modal3Active = ref(false);
 </script>
 
 <template>
@@ -11,9 +12,19 @@ let modal2Active = ref(false);
     <div class="content">
       <button class="button is-primary" @click="modal1Active = true">Open Modal 1</button>
       <button class="button is-link" @click="modal2Active = true">Open Modal 2</button>
+      <button class="button is-warning" @click="modal3Active = true">Open Modal 3</button>
 
       <Modal :active="modal1Active" @close="modal1Active = false" url="https://picsum.photos/1290/960?random=1"> </Modal>
-      <Modal :active="modal2Active" @close="modal1Active = false" url="https://picsum.photos/1290/960?random=2"> </Modal>
+      <Modal :active="modal2Active" @close="modal2Active = false" url="https://picsum.photos/1290/960?random=2"> </Modal>
+      <Modal :active="modal3Active" @close="modal3Active = false" >
+      <div class="card">
+  <div class="card-content">
+    <div class="content">
+      Kass ehk kodukass on kaslaste sugukonna kassi perekonda kuuluv väike kiskja, kaslaste hulgas ainus koduloom. Kodukasse arvatakse maailmas elavat 600 miljonit ehk rohkem kui kõiki teisi kaslasi kokku.
+    </div>
+  </div>
+</div>
+      </Modal>
     
     </div>
   </div>
